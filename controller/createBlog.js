689fb949f89b36e686aa00673de836326f9cefc1
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-06-22 15:24:59 +0100

    routes & create func

--- controller/createBlog.js
+++ controller/createBlog.js
@@ -1,14 +1,11 @@
 const BlogPost = require("../model/blogSchema");
 
-const createBlogPost = (req, res) => {
+const createBlogPost = async (req, res) => {
   const { title, content, author } = req.body;
-  const blogPost = new BlogPost({ title, content, author });
-  blogPost.save();
-  console.log(blogPost)
+  const blogPost = await BlogPost.create({ title, content, author });
   res.json(blogPost);
-  
 };
 
 module.exports = {
-  createBlogPost
+  createBlogPost,
 };
